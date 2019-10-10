535c93a9032a995705f7adf0172bbef7f298dc3c
Sasi Kumar T <32474530+Sasikumar3096@users.noreply.github.com>
2019-10-10 09:18:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,59 @@
 from flask import Flask
 from datetime import datetime
+import psycopg2
+from bs4 import BeautifulSoup as bs
+import requests
+import xlrd
+import json
+
 app = Flask(__name__)
 
+
+table_name='final_table'
+
+
+def get_datas(cursor):
+    
+    select_query = "SELECT * FROM "+table_name+";"
+    cursor.execute(select_query)
+    datas = cursor.fetchall()
+    return datas
+
+def update_data(url,id,cursor):
+    
+    page = requests.get(url)
+    page = bs(page.text,'html.parser')
+       
+    try:
+        results=[]
+        for data in page.findAll('div',{'class':'user-information__achievements-data'}):
+            results.append(data.contents[0])
+        badges = results[0]
+        points = (results[1][1:-1]).replace(",","")
+        trails = results[2][1:-1]
+        update_query = "UPDATE "+table_name+" SET badges= "+badges+" ,points="+points+", trails="+trails+" WHERE id ='"+str(id)+"';"
+        print(update_query)
+        cursor.execute(update_query)
+    except Exception:
+        print("Error")
+        update_data(url,id,cursor)
+		
+
 @app.route('/')
-def homepage():
-    the_time = datetime.now().strftime("%A, %d %b %Y %l:%M %p")
-
-    return """
-    <h1>Hello heroku</h1>
-    <p>It is currently {time}.</p>
-
-    <img src="http://loremflickr.com/600/400" />
-    """.format(time=the_time)
+def update():
+    connection = psycopg2.connect(user = "zankzcqmyuheau",
+                                  password = "b3913eb5fdb660bd936979c8092adc0c2bef6b1294caf4b2395d5d387f511407",
+                                  host = "ec2-46-137-113-157.eu-west-1.compute.amazonaws.com",
+                                  port = "5432",
+                                  database = "d5de9d996c3btd")
+    cursor = connection.cursor()
+    datas = get_datas(cursor)
+    for i in datas:
+        url,id=i[-1],i[0]
+        update_data(url,id,cursor)
+    connection.commit()
+    connection.close()
+    return {'message':'success'},200
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
